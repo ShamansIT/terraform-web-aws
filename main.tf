5f14dbf0680a445f50475ea931331c07d1f60847
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2025-12-02 13:01:45 +0000

    Add Application Load Balancer (Stage 8)

--- main.tf
+++ main.tf
@@ -273,3 +273,86 @@ resource "aws_instance" "web_b" {
     AZ          = data.aws_availability_zones.available.names[1]
   }
 }
+
+# ***************************************************
+# Application Load Balancer
+# ***************************************************
+
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  # Public subnets for ALB
+  subnets = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+
+  security_groups = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name        = "web-alb"
+    Component   = "load-balancer"
+    Environment = "lab"
+  }
+}
+
+# ***************************************************
+# Target Group for web instances
+# ***************************************************
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    protocol            = "HTTP"
+    path                = "/"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name        = "web-tg"
+    Component   = "web"
+    Environment = "lab"
+  }
+}
+
+# ***************************************************
+# Attach web instances to Target Group
+# ***************************************************
+
+resource "aws_lb_target_group_attachment" "web_a_attachment" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web_a.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web_b_attachment" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web_b.id
+  port             = 80
+}
+
+# ***************************************************
+# ALB Listener (HTTP :80)
+# ***************************************************
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,7 @@ output "web_b_public_ip" {
   value       = aws_instance.web_b.public_ip
 }
 
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.web_alb.dns_name
+}
